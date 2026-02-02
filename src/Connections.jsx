bd375f396c27e80666fe1be7c099fb87accf2205
zach <zbernn@gmail.com>
2026-02-01 23:05:06 -0800

    added selection logic

--- src/Connections.jsx
+++ src/Connections.jsx
@@ -1,18 +1,26 @@
 import "./Connections.css";
+import { useState } from 'react'
 
 export default function Connections() {
+  const [selectedTiles, setSelectedTiles] = useState([])
   const words = [
     "BE", "MY", "VALENTINE", "?",
-    "DOG", "CAT", "MOUSE", "HORSE",
-    "RED", "BLUE", "GREEN", "YELLOW",
-    "CAR", "TRAIN", "PLANE", "BOAT"
+    "DOG", "JORDAN", "DEROZAN", "HORSE",
+    "RED", "SEE", "GREEN", "LAVINE",
+    "CAR", "TRAIN", "ARE", "BOAT"
   ];
-
+  const handleSelect = (idx) => {
+    if (!selectedTiles.includes(idx) && selectedTiles.length !== 4) {
+      setSelectedTiles([...selectedTiles, idx]);
+    } else {
+      setSelectedTiles(selectedTiles.filter(i => i !== idx));
+    }
+  }
   return (
     <div className="connections-page">
       <div className="grid">
         {words.map((word, idx) => (
-          <div key={idx} className="tile">
+          <div key={idx} className={selectedTiles.includes(idx) ? "selectedTile" : "tile"} onClick={() => handleSelect(idx)}>
             {word}
           </div>
         ))}
